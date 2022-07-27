653f53aaa577de9a841a54d971084e6da160a4c3
mppc1985 <marcospaulo.cerqueira@gmail.com>
2022-07-27 00:34:10 +0000

    aula3

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template #importar biblioteca do flask e chamar a Classe Fask e a função render template que é do Flask
+from datetime import datetime   
 
 app=Flask("hello") #nome da aplicação
 
-@app.route("/") #raiz da aplicação 
-@app.route("/hello") #cria as paginas
-def hello():
-    return "Hello Word"
+#lista > dicionario
+posts= [ #mock = substituto, simulacao
+    { #isso é um post
+        "title": "O meu primeiro post",
+        "body": "Aqui é o texto do Post",
+        "author": "Marcos",
+        "created": datetime(2022,7,25)
+    },
+        { #isso é um post
+        "title": "O meu segundo post",
+        "body": "Aqui é o texto do Post",
+        "author": "danilo",
+        "created": datetime(2022,7,26)
+    },
 
-@app.route("/meucontato") #cria a pagina meu contato
-def meuContato():
-    return render_template('index.html')
+]
+
+
+@app.route("/") #raiz da aplicação 
+def index():
+    return render_template("index.html", posts=posts)
+
