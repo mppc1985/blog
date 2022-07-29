78a6b81a748b8d56f2cb0a2bb3bbcbf6589114c6
mppc1985 <marcospaulo.cerqueira@gmail.com>
2022-07-29 23:59:55 +0000

    aula6

--- app.py
+++ app.py
@@ -1,58 +1,59 @@
-from flask import Flask, render_template, redirect, url_for, request, flash #importar biblioteca do flask e chamar a Classe Fask e a função render template que é do Flask, request pega o metodo da requisição
+from flask import Flask, render_template, redirect, url_for, request, flash
 from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user
-from werkzeug.security import check_password_hash,generate_password_hash
+from flask_login import LoginManager, UserMixin, current_user, logout_user, login_user, login_required
+from werkzeug.security import check_password_hash, generate_password_hash
 from sqlalchemy.exc import IntegrityError
+import os
 
-app=Flask("hello") #nome da aplicação
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app = Flask("hello")
+db_url= os.environ.get("DATABASE_URL") or "sqlite:///app.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = db_url.replace("postgres", "postgresql")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SECRET_KEY"]="pudim"
+app.config["SECRET_KEY"] = "pudim"
 
-db = SQLAlchemy(app)#chama o BD e manda o APP 
-login = LoginManager(app) #ajuda a criar o login
+db = SQLAlchemy(app)
+login = LoginManager(app)
 
-
-class Post(db.Model): #Codigo em py que explica como criar a tabela do DB, se não falar nada ele cria a tabela com nome de Post
-    __tablename__='posts' #nome a tabela para não deixar automatico, nome em minuscula e no plural 
-    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title=db.Column(db.String(70), nullable=False)
-    body=db.Column(db.String(500))
-    created=db.Column(db.DateTime, nullable=False, default=datetime.now)
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(70), nullable=False)
+    body = db.Column(db.String(500))
+    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
 
 class User(UserMixin, db.Model):
-    __tablename__="users"
-    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username=db.Column(db.String(20), nullable=False, unique=True, index=True)#index pq vamos buscar muito no banco
-    email=db.Column(db.String(64), nullable=False, unique=True)
-    password_hash=db.Column(db.String(128), nullable=False)
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(20), nullable=False, unique=True, index=True) 
+    email = db.Column(db.String(64), nullable=False, unique=True)
+    password_hash = db.Column(db.String(128), nullable=False)
     posts = db.relationship('Post', backref='author')
 
     def set_password(self, password):
-        self.password_has=generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return check_password_hash(self.password_has, password)
+        return check_password_hash(self.password_hash, password)
 
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
 
 
-db.create_all() #cria as tabelas que não tem no banco, se tiver não cria novo
+db.create_all()
 
-@app.route("/") #raiz da aplicação 
-def index():    
-    posts=Post.query.all()
+@app.route("/")
+def index():
+    posts = Post.query.all()
     return render_template("index.html", posts=posts)
 
 @app.route('/register', methods=["GET","POST"])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    if request.method== "POST":
+    if request.method == "POST":
         username = request.form['username']
         email = request.form['email']
         password = request.form['password']
@@ -61,22 +62,20 @@ def register():
             new_user.set_password(password)
             db.session.add(new_user)
             db.session.commit()
-        except IntegrityError:  
-            flash("username or e-mail already exists!") 
-        else:    
+        except IntegrityError:
+            flash("Username or E-mail already exists!")
+        else:
             return redirect(url_for('login'))
-
-
     return render_template('register.html')
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
-    if request.method=='POST':
-        username=request.form['username']
-        password=request.form['password']
-        user=User.query.filter_by(username=username).first()
+    if request.method == "POST":
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
         if user is None or not user.check_password(password):
             flash("Incorrect Username or Password")
             return redirect(url_for('login'))
@@ -89,3 +88,19 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/create', methods=["GET", "POST"])
+@login_required
+def create():
+    if request.method == "POST":
+        title=request.form['title']
+        body=request.form['body']
+
+        try:
+            post=Post(title=title, body=body, author=current_user)
+            db.session.add(post)
+            db.session.commit()
+            return redirect(url_for('index'))
+        except IntegrityError:
+            flash("Error on create Post")
+    return render_template('create.html')
